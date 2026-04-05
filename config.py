0349dc62633253dca34980801fac7f02b7282b90
kenshinxu4 <kenshinxu.4@gmail.com>
2026-04-05 14:40:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 import os
-from telethon.sessions import StringSession
 
 API_ID = int(os.environ.get("API_ID", 0))
 API_HASH = os.environ.get("API_HASH", "")
@@ -7,5 +6,5 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 ADMIN_ID = int(os.environ.get("ADMIN_ID", 0))
 SESSION = os.environ.get("SESSION", "")
 
-OWNER_USERNAME = "@KENSHIN_ANIME_OWNER"
-BOT_NAME = "KENSHIN ANIME SC BOT"
+OWNER_USERNAME = "@KENSHIN_ANIME"
+BOT_NAME = "KENSHIN ANIME BOT"
